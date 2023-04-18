4059e88cd0d0f3761e6f59437b2a97f0e654cef2
NullPointer1331 <130084942+NullPointer1331@users.noreply.github.com>
2023-04-17 21:46:07 -0700

    added code for password and phone number validation

--- ts/main.ts
+++ ts/main.ts
@@ -7,8 +7,13 @@ window.onload = function() {
 }
 
 function main():void {
-    isTextPresent("first_name", "First name is required");
-    isTextPresent("last_name", "Last name is required");
+    let validForm:boolean = (isTextPresent("first_name", "First name is required") &&
+    isTextPresent("last_name", "Last name is required") &&
+    isEmailValid() &&
+    isPasswordValid() &&
+    isPhoneNumberValid());
+    if (validForm) {
+    }
 }
 
 /**
@@ -28,16 +33,49 @@ function isTextPresent(id:string, errMsg:string):boolean {
 }
 
 function isEmailValid():boolean {
-    let emailBox:HTMLInputElement = $("email");
-    let email:string = emailBox.value;
-    if (email == "") {
-        let errSpan:HTMLElement = <HTMLElement>emailBox.nextElementSibling;
-        errSpan.innerText = "Email is required";
+    if (!isTextPresent("email", "Email is required")) {
         return false;
     }
+    let emailBox:HTMLInputElement = $("email");
+    let email:string = emailBox.value;
     if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
         let errSpan:HTMLElement = <HTMLElement>emailBox.nextElementSibling;
         errSpan.innerText = "Invalid email";
+        return false;
+    }
+    return true;
+}
+
+function isPasswordValid():boolean {
+    if(!isTextPresent("password", "Password is required")) {
+        return false;
+    }
+    let passwordBox:HTMLInputElement = $("password");
+    let password:string = passwordBox.value;
+    if (password.length < 6) {
+        let errSpan:HTMLElement = <HTMLElement>passwordBox.nextElementSibling;
+        errSpan.innerText = "Password must be at least 6 characters";
+        return false;
+    }
+    let passwordBox2:HTMLInputElement = $("verify");
+    let password2:string = passwordBox2.value;
+    if (password != password2) {
+        let errSpan:HTMLElement = <HTMLElement>passwordBox.nextElementSibling;
+        errSpan.innerText = "Passwords don't match";
+        return false;
+    }
+    return true;
+}
+
+function isPhoneNumberValid():boolean {
+    if (!isTextPresent("phone", "Phone number is required")) {
+        return false;
+    }
+    let phoneBox:HTMLInputElement = $("phone");
+    let phone:string = phoneBox.value;
+    if (!/^\d{3}-\d{3}-\d{4}$/.test(phone)) {
+        let errSpan:HTMLElement = <HTMLElement>phoneBox.nextElementSibling;
+        errSpan.innerText = "Invalid phone number";
         return false;
     }
     return true;
